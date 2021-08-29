4d6d33790409e121b6ee3449c4010252f7bf87d1
kr-abhishek3031 <kumar.abhishek3031@gmail.com>
2021-08-29 11:46:14 +0530

    Added positive and negative tests for searchProduct by ID

--- javaCourseChallenge/src/test/java/p5/InventoryTest.java
+++ javaCourseChallenge/src/test/java/p5/InventoryTest.java
@@ -1,15 +1,33 @@
 package p5;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InventoryTest {
     static Inventory inventory = new Inventory();
 
-    @Test
-    public void searchProductTest(){
-        Integer i = 1;
-        assertEquals(true, inventory.searchProduct(1));
+    @BeforeAll
+    public static void populateInventory(){
+        inventory.productList.put(4, new Product(4, "Hike", 1000.0));
+        inventory.productList.put(5, new Product(5, "Reebok", 2000.0));
+        inventory.productList.put(6, new Product(6, "Uniqlo", 3000.0));
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6}) // 3
+    public void searchProductByIDPositveTest(int id){
+        assertEquals(true, inventory.searchProduct(id));
+    }
+    @ParameterizedTest
+    @ValueSource(ints = { 7,8, 9, 10, Integer.MAX_VALUE}) // 3
+    public void searchProductByIDnegativeTest(int id){
+        assertEquals(false, inventory.searchProduct(id));
+    }
+
+
 }
